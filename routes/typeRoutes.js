45ca07e4cc35383dee20a434011fda74e9ed8034
Eugene Zarkhin <zarkhineugene@gmail.com>
2018-04-20 01:29:19 +0300

    Add routes structure

---
+++ routes/typeRoutes.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+const Type = mongoose.model('Type')
+const Unit = mongoose.model('Unit')
+
+module.exports = app => {
+  
+}
---
+++ routes/workerRoutes.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+const Worker = mongoose.model('Worker')
+const Unit = mongoose.model('Unit')
+
+module.exports = app => {
+
+}
